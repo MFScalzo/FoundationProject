4a6dddabd5d87df18312da37819de3d9e95e63ed
Matt Scalzo <mattcrunch@gmail.com>
2022-03-19 22:54:04 -0400

    Create mDBControl, can now view account info

--- cli.py
+++ cli.py
@@ -1,7 +1,17 @@
+from mDBControl import viewAc
+
 def menu():
-    print("1. Load")
-    print("2. Show")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Please choose an option:")
+    print("1. Create Account")
+    print("2. View Account")
+    print("3. Deposit")
+    print("4. Withdarw")
+    print("5. Transfer")
+    print("6. Delete Account")
+    print("7. Load Account")
     print("0. Exit")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     
     try: 
         option = int(input("Select menu option: "))
@@ -12,6 +22,10 @@ def menu():
     return option
 
 def main():
+    print("########################################")
+    print("    Welcome to the Bank Teller CLI!")
+    print("########################################")
+
     while(True):
         optionSelected = menu()
         if(optionSelected == 0):
@@ -19,11 +33,48 @@ def main():
             break
 
         if(optionSelected == 1):
-            print("You selected option 1 to load data...")
+            print("You selected \"Create Account\".")
+            pass        # call create function here
+
         elif(optionSelected == 2):
-            print("You selected option 2 to show data...")
+            print("You selected \"View Account\".")
+            try:
+                num = int(input("Enter account number: "))
+                account = viewAc(num)       # viewAc is trying to find the record in the DB with the given account number
+            
+            except:
+                account = None
+
+            if(account):
+                print(f"Account Number: {account['accountNumber']}")
+                print(f"Name: {account['name']}")
+                print(f"Balance: ${account['balance']}")
+
+            else:
+                print(f"Could not find account number: {num}")
+
+        elif(optionSelected == 3):
+            print("You selected \"Deposit\".")
+            pass        # call Deposit function here
+
+        elif(optionSelected == 4):
+            print("You selected \"Withdraw\".")
+            pass        # call Withdraw function here
+
+        elif(optionSelected == 5):
+            print("You selected \"Transfer\".")
+            pass        # call Transfer function here
+
+        elif(optionSelected == 6):
+            print("You selected \"Delete Account\".")
+            pass        # call Delete function here
+
+        elif(optionSelected == 7):
+            print("You selected \"Load Account\".")
+            pass        # call Load function here
+
         else:
-            print("Invalid option. Try again...")
+            print("Invalid option. Try again.")
 
 if __name__ == "__main__":
     main()
--- mDBControl.py
+++ mDBControl.py
@@ -4,3 +4,9 @@ client = MongoClient()
 
 db = client.foundation
 
+def viewAc(accNum):
+    # May have to add an error check for incorect input, or do it above, not sure
+    return db.accounts.find_one({'accountNumber': accNum})
+
+
+#print(type(viewAc(100)))
