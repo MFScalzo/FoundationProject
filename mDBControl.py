dbf64165188cd7b2e4a918bea144747b6dfad649
MFScalzo <100707377+MFScalzo@users.noreply.github.com>
2022-03-30 17:05:16 -0400

    Remove comment

--- mDBControl.py
+++ mDBControl.py
@@ -7,7 +7,6 @@ client = MongoClient()
 db = client.foundation
 
 def viewAccount(accountNumber):
-    # May have to add an error check for incorect input, or do it above, not sure
     return db.accounts.find_one({'accountNumber': accountNumber})
 
 def loadAccount(filename):
